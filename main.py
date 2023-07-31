3110b9599713668679b1202f252bd82638229da7
Rui Catarino <rui.catarino@fabamaq.com>
2023-07-31 14:52:54 +0100

    Update sorte and add return_dict_rand()

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
+import random
+from datetime import datetime
 from fastapi import FastAPI
 
 app = FastAPI()
+
+sorte = [
+    {
+        "item": "SORTE",
+        "probability": 0.3,
+        "description": "Estas livre da prisao!\nGuarda esta carta para uso proprio ou podes oferecer/vender a outro jogar por goles.",
+    },
+    {
+        "item": "IMUNIDADE",
+        "probability": 0.2,
+        "description": "A partir de este momento estas safo de um desafio a tua escolha.",
+    },
+    {
+        "item": "REVERSO",
+        "probability": 0.2,
+        "description": "Usa esta carta para te livrares de um desafio e escolhe outro jogador para faze-lo por ti.",
+    },
+    {
+        "item": "SORTE GRANDE",
+        "probability": 0.3,
+        "description": "Avanca tres casas!",
+    },
+]
+
+azar = []
+
+misterio = []
 
 
 @app.get("/")
@@ -15,9 +44,18 @@ def get_azar():
 
 @app.get("/sorte")
 def get_sorte():
-    return {}
+    choice = return_dict_rand(sorte)
+    print{choice["item"]:choice["description"]}
 
 
 @app.get("/misterio")
 def get_misterio():
     return {}
+
+
+def return_dict_rand(dicty):
+    random.seed(datetime.now().timestamp())
+    probabilities = [item["probability"] for item in dicty]
+    random_index = random.choices(range(len(dicty)), weights=probabilities)[0]
+    chosen_item_dicty = dicty[random_index]
+    return chosen_item_dicty
