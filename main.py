7dee36a3cb1340f269c5afba91e18b9d316ecdc7
Rui Catarino <rui.catarino@fabamaq.com>
2023-08-01 14:53:02 +0100

    Add duration

--- main.py
+++ main.py
@@ -10,36 +10,43 @@ sorte = [
         "item": "SORTE",
         "probability": 0.05,
         "description": "Estás livre da prisão! Guarda esta carta para uso próprio ou podes oferecer/vender a outro jogar por goles.",
+        "duration": "Até usar",
     },
     {
         "item": "IMUNIDADE",
         "probability": 0.1,
         "description": "A partir de este momento estás safo de um desafio à tua escolha.",
+        "duration": "Um desafio",
     },
     {
         "item": "REVERSO",
         "probability": 0.185,
         "description": "Usa esta carta para te livrares de um desafio e escolhe outro jogador para fazê-lo por ti.",
+        "duration": "Um desafio",
     },
     {
         "item": "SORTE GRANDE",
         "probability": 0.185,
         "description": "Avança três casas!",
+        "duration": "Imediata",
     },
     {
         "item": "PASSA À FRENTE",
         "probability": 0.185,
         "description": "Nos próximos dois desafios, caso o teu parceiro de jogo tenha de beber, tu não precisas de o fazer!",
+        "duration": "Dois desafios",
     },
     {
         "item": "AMIGO SORTUDO",
         "probability": 0.11,
-        "description": "Escolhe um jogador para beber o dobro nesta rodada.",
+        "description": "Escolhe um jogador para beber o dobro.",
+        "duration": "Uma ronda",
     },
     {
         "item": "ÉS O ESCOLHIDO",
         "probability": 0.185,
-        "description": "Todos os jogadores bebem um gole extra durante esta ronda, mas tu não.",
+        "description": "Todos os jogadores bebem um gole extra, tu não.",
+        "duration": "Uma ronda",
     },
 ]
 
@@ -48,36 +55,43 @@ azar = [
         "item": "ESTÁS COM SEDE",
         "probability": 0.05,
         "description": "Pede aos outros jogadores para te trazerem um shot de uma bebida alcoólica à escolha deles.",
+        "duration": "Imediata",
     },
     {
         "item": "TICK TACK",
         "probability": 0.05,
         "description": "A partir deste momento tens exatamente dois minutos para terminares o teu copo. Boa sorte!",
+        "duration": "Imediata",
     },
     {
         "item": "VAIS DENTRO",
         "probability": 0.18,
         "description": "Vai diretamente para a casa da prisão. Azarito!",
+        "duration": "Imediata",
     },
     {
         "item": "GRANDE AZAR",
         "probability": 0.18,
         "description": "Volta três casas atrás.",
+        "duration": "Imediata",
     },
     {
         "item": "ROLA OUTRA VEZ",
         "probability": 0.18,
         "description": "Joga os dados de novo. O número que sair são as casas que tens de voltar atrás!",
+        "duration": "Imediata",
     },
     {
         "item": "AZAR NO BARALHO",
         "probability": 0.18,
         "description": "Bebe dois goles e tira outa carta!",
+        "duration": "Imediata",
     },
     {
         "item": "DISCURSO AZARADO",
         "probability": 0.18,
-        "description": "Inventa uma catchphrase que terás de dizer sempre antes de beber (durante duas rondas)!",
+        "description": "Inventa uma catchphrase que terás de dizer sempre antes de beber!",
+        "duration": "Duas rondas",
     },
 ]
 
@@ -86,41 +100,49 @@ misterio = [
         "item": "MONARQUIA",
         "probability": 0.04,
         "description": "Podes eliminar todas as regras existentes e substituir pelas que quiseres.",
+        "duration": "Imediata",
     },
     {
         "item": "ÉS TU QUE MANDAS",
         "probability": 0.15,
         "description": "Tens a possibilidade de criar uma regra e eliminar outra!",
+        "duration": "Imediata",
     },
     {
         "item": "QUE CHATICE",
         "probability": 0.15,
         "description": "Recua duas casas!",
+        "duration": "Imediata",
     },
     {
         "item": "NEM QUERIAS",
         "probability": 0.15,
         "description": "Avança duas casas.",
+        "duration": "Imediata",
     },
     {
         "item": "A ESCOLHA É TUA",
         "probability": 0.15,
         "description": "Caso tenhas parceiro(s) de jogo, tens a opção de sair do grupo.",
+        "duration": "Imediata",
     },
     {
         "item": "MISTÉRIO DO DESTINO",
         "probability": 0.15,
         "description": "Atira uma moeda ao ar. Se sair cara escolhe alguem para beber, se sair coroa bebes tu.",
+        "duration": "Imediata",
     },
     {
         "item": "SORTE ENCANTADA",
         "probability": 0.06,
-        "description": "Ganha um bônus de sorte: escolhe a bebida que quiseres (sim pode ser água) para beber durante esta ronda.",
+        "description": "Ganha um bônus de sorte: escolhe a bebida que quiseres (sim pode ser água) para beber.",
+        "duration": "Uma ronda",
     },
     {
         "item": "PENA ESCONDIDA",
         "probability": 0.15,
         "description": "Escolhe uma pessoa e pensa num número de 1 a 5. A pessoa terá de beber penalidades e no fim dizer quantas bebeu, se acertar acabou se não volta a beber até adivinhar o número.",
+        "duration": "Imediata",
     },
 ]
 
@@ -132,30 +154,37 @@ def welcome():
 
 @app.get("/azar")
 def get_azar():
-    html_content = return_dict_parse(azar,"https://i.ibb.co/9h1v5LK/bad-luck-1176087376.jpg")
+    html_content = return_dict_parse(
+        azar, "https://i.ibb.co/9h1v5LK/bad-luck-1176087376.jpg"
+    )
     return HTMLResponse(content=html_content, status_code=200)
 
 
 @app.get("/sorte")
 def get_sorte():
-    html_content = return_dict_parse(sorte,"https://i.ibb.co/gr90HJJ/sorte.png")
+    html_content = return_dict_parse(sorte, "https://i.ibb.co/gr90HJJ/sorte.png")
     return HTMLResponse(content=html_content, status_code=200)
 
 
 @app.get("/misterio")
 def get_misterio():
-    html_content = return_dict_parse(misterio,"https://i.ibb.co/1msVT88/mistery.png")
+    html_content = return_dict_parse(misterio, "https://i.ibb.co/1msVT88/mistery.png")
     return HTMLResponse(content=html_content, status_code=200)
 
 
-def return_dict_parse(dicty,icon_url):
+def return_dict_parse(dicty, icon_url):
     probabilities = [item["probability"] for item in dicty]
     random_index = random.choices(range(len(dicty)), weights=probabilities)[0]
     chosen_item_dicty = dicty[random_index]
-    return html_parse(chosen_item_dicty["item"], chosen_item_dicty["description"],icon_url)
+    return html_parse(
+        chosen_item_dicty["item"],
+        chosen_item_dicty["description"],
+        chosen_item_dicty["duration"],
+        icon_url,
+    )
 
 
-def html_parse(item, description,icon_url):
+def html_parse(item, description, duration, icon_url):
     return f"""
     <!DOCTYPE html>
     <html>
@@ -173,6 +202,7 @@ def html_parse(item, description,icon_url):
                 <div class="card-body">
                     <h5 class="card-title">{item}</h5>
                     <p class="card-text">{description}</p>
+                    <p class="card-text"><small class="text-muted">Duração: {duration}</small></p>
                 </div>
             </div>
             <div class="d-flex justify-content-center mt-3">
@@ -185,6 +215,7 @@ def html_parse(item, description,icon_url):
     </body>
     </html>
     """
+
 
 def html_main():
     return f"""
