6fa828a577189da955095674144f0ba56b255275
Rui Catarino <55796280+ruitcatarino@users.noreply.github.com>
2023-08-01 02:37:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 sorte = [
     {
         "item": "SORTE",
-        "probability": 0.1,
+        "probability": 0.05,
         "description": "Estás livre da prisão! Guarda esta carta para uso próprio ou podes oferecer/vender a outro jogar por goles.",
     },
     {
@@ -18,74 +18,109 @@ sorte = [
     },
     {
         "item": "REVERSO",
-        "probability": 0.3,
+        "probability": 0.185,
         "description": "Usa esta carta para te livrares de um desafio e escolhe outro jogador para fazê-lo por ti.",
     },
     {
         "item": "SORTE GRANDE",
-        "probability": 0.2,
+        "probability": 0.185,
         "description": "Avança três casas!",
     },
     {
         "item": "PASSA À FRENTE",
-        "probability": 0.3,
+        "probability": 0.185,
         "description": "Nos próximos dois desafios, caso o teu parceiro de jogo tenha de beber, tu não precisas de o fazer!",
+    },
+    {
+        "item": "AMIGO SORTUDO",
+        "probability": 0.11,
+        "description": "Escolhe um jogador para beber o dobro nesta rodada.",
+    },
+    {
+        "item": "ÉS O ESCOLHIDO",
+        "probability": 0.185,
+        "description": "Todos os jogadores bebem um gole extra durante esta ronda, mas tu não.",
     },
 ]
 
 azar = [
     {
         "item": "ESTÁS COM SEDE",
-        "probability": 0.1,
+        "probability": 0.05,
         "description": "Pede aos outros jogadores para te trazerem um shot de uma bebida alcoólica à escolha deles.",
     },
     {
         "item": "TICK TACK",
-        "probability": 0.1,
+        "probability": 0.05,
         "description": "A partir deste momento tens exatamente dois minutos para terminares o teu copo. Boa sorte!",
     },
     {
         "item": "VAIS DENTRO",
-        "probability": 0.2,
+        "probability": 0.18,
         "description": "Vai diretamente para a casa da prisão. Azarito!",
     },
     {
         "item": "GRANDE AZAR",
-        "probability": 0.3,
+        "probability": 0.18,
         "description": "Volta três casas atrás.",
     },
     {
         "item": "ROLA OUTRA VEZ",
-        "probability": 0.3,
+        "probability": 0.18,
         "description": "Joga os dados de novo. O número que sair são as casas que tens de voltar atrás!",
+    },
+    {
+        "item": "AZAR NO BARALHO",
+        "probability": 0.18,
+        "description": "Bebe dois goles e tira outa carta!",
+    },
+    {
+        "item": "DISCURSO AZARADO",
+        "probability": 0.18,
+        "description": "Inventa uma catchphrase que terás de dizer sempre antes de beber (durante duas rondas)!",
     },
 ]
 
 misterio = [
     {
         "item": "MONARQUIA",
-        "probability": 0.1,
+        "probability": 0.04,
         "description": "Podes eliminar todas as regras existentes e substituir pelas que quiseres.",
     },
     {
         "item": "ÉS TU QUE MANDAS",
-        "probability": 0.2,
+        "probability": 0.15,
         "description": "Tens a possibilidade de criar uma regra e eliminar outra!",
     },
     {
         "item": "QUE CHATICE",
-        "probability": 0.3,
+        "probability": 0.15,
         "description": "Recua duas casas!",
     },
     {
         "item": "NEM QUERIAS",
-        "probability": 0.2,
+        "probability": 0.15,
         "description": "Avança duas casas.",
     },
     {
         "item": "A ESCOLHA É TUA",
-        "probability": 0.2,
+        "probability": 0.15,
         "description": "Caso tenhas parceiro(s) de jogo, tens a opção de sair do grupo.",
+    },
+    {
+        "item": "MISTÉRIO DO DESTINO",
+        "probability": 0.15,
+        "description": "Atira uma moeda ao ar. Se sair cara escolhe alguem para beber, se sair coroa bebes tu.",
+    },
+    {
+        "item": "SORTE ENCANTADA",
+        "probability": 0.06,
+        "description": "Ganha um bônus de sorte: escolhe a bebida que quiseres (sim pode ser água) para beber durante esta ronda.",
+    },
+    {
+        "item": "PENA ESCONDIDA",
+        "probability": 0.15,
+        "description": "Escolhe uma pessoa e pensa num número de 1 a 5. A pessoa terá de beber penalidades e no fim dizer quantas bebeu, se acertar acabou se não volta a beber até adivinhar o número.",
     },
 ]
 
@@ -167,7 +202,7 @@ def html_main():
                 <h2 class="my-4">Welcome to the best Drinking Game, Pitos!</h2>
                 <div class="row justify-content-center">
                     <div class="col-lg-4 col-md-6 col-sm-6 col-12" style="margin-bottom: 1rem;">
-                        <div class="card" style="width: 18rem;">
+                        <div class="card text-center border-success mb-3" style="width: 18rem;">
                             <a href="https://boardgame-1-s6567828.deta.app/sorte">
                                 <img class="card-img-top" src="https://i.ibb.co/gr90HJJ/sorte.png" alt="Sorte">
                             </a>
@@ -177,7 +212,7 @@ def html_main():
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-6 col-sm-6 col-12" style="margin-bottom: 1rem;">
-                        <div class="card" style="width: 18rem;">
+                        <div class="card text-center border-danger mb-3" style="width: 18rem;">
                             <a href="https://boardgame-1-s6567828.deta.app/azar">
                                 <img class="card-img-top" src="https://i.ibb.co/9h1v5LK/bad-luck-1176087376.jpg" alt="Azar">
                             </a>
@@ -187,7 +222,7 @@ def html_main():
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-6 col-sm-6 col-12" style="margin-bottom: 1rem;">
-                        <div class="card" style="width: 18rem;">
+                        <div class="card text-center border-dark mb-3" style="width: 18rem;">
                             <a href="https://boardgame-1-s6567828.deta.app/misterio">
                                 <img class="card-img-top" src="https://i.ibb.co/1msVT88/mistery.png" alt="Mistério">
                             </a>
